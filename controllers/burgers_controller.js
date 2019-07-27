20d1023fd103804cb15d24fbe28843bd0a0e308e
J <j.mey.r8@gmail.com>
2019-07-26 21:09:58 -0400

    models updated

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,45 @@
 //Express require
 var express = require("express");
 
+var router = express.Router();
+
 var burger = require("../models/burger.js");
 
-var router = express.Router();
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+      "burger_name"
+    ], [
+      req.body.burger_name
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.updateOne({
+        devoured: true
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 
 module.exports = router;
