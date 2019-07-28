6e9f88fbe6ac12e1aff68b33d3be9628c62626d3
J <j.mey.r8@gmail.com>
2019-07-28 01:48:54 -0400

    fixed env file

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
-// MySQL connection
+require("dotenv").config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+        connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+
+};
 
 // Make a connection
 connection.connect(function(err) {
@@ -19,4 +26,5 @@ connection.connect(function(err) {
 });
 
 // Export the connection
+
 module.exports = connection;
